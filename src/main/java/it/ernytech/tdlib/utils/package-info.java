5c4a1d211f99e84670a40c9541cb0f505d86ac9c
ErnyTech <erny.castell@gmail.com>
2018-07-18 17:17:37 +0200

    Updated docs for package description

---
+++ src/main/java/it/ernytech/tdlib/utils/package-info.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2018. Ernesto Castellotti <erny.castell@gmail.com>
+ * This file is part of JTdlib.
+ *
+ *     JTdlib is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     JTdlib is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with JTdlib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * it.ernytech.tdlib.utils is a group of utils necessary to complete the essential operation of tdlib.
+ */
+package it.ernytech.tdlib.utils;
