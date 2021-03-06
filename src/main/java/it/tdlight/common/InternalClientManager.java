ad8d48ecb1ae95b201c7b743a8be6dd893884552
Andrea Cavalli <andrea@cavallium.it>
2021-03-06 17:27:08 +0100

    Filter out generated error messages during shutdown

--- src/main/java/it/tdlight/common/InternalClientManager.java
+++ src/main/java/it/tdlight/common/InternalClientManager.java
@@ -1,6 +1,12 @@
 package it.tdlight.common;
 
 import it.tdlight.jni.TdApi;
+import it.tdlight.jni.TdApi.Object;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
@@ -37,17 +43,43 @@ public class InternalClientManager implements AutoCloseable {
 		if (handler != null) {
 			handler.handleEvents(isClosed, clientEventIds, clientEvents);
 		} else {
-			logger.error("Unknown client id \"{}\"! {} events have been dropped!", clientId, clientEvents.length);
-			for (int i = 0; i < clientEvents.length; i++) {
-				long id = clientEventIds[i];
-				TdApi.Object event = clientEvents[i];
-				logger.error("The following event, with id \"{}\", has been dropped: {}", id, event);
+			java.util.List<Entry<Long, TdApi.Object>> droppedEvents = getEffectivelyDroppedEvents(clientEventIds, clientEvents);
+
+			if (!droppedEvents.isEmpty()) {
+				logger.error("Unknown client id \"{}\"! {} events have been dropped!", clientId, droppedEvents.size());
+				for (Entry<Long, Object> droppedEvent : droppedEvents) {
+					logger.error("The following event, with id \"{}\", has been dropped: {}",
+							droppedEvent.getKey(),
+							droppedEvent.getValue());
+				}
 			}
 		}
 
 		if (isClosed) {
 			registeredClientEventHandlers.remove(clientId);
 		}
+	}
+
+	/**
+	 * Get only events that have been dropped, ignoring synthetic errors related to the closure of a client
+	 */
+	private List<Entry<Long, TdApi.Object>> getEffectivelyDroppedEvents(long[] clientEventIds, TdApi.Object[] clientEvents) {
+		java.util.List<Entry<Long, TdApi.Object>> droppedEvents = new ArrayList<>(clientEvents.length);
+		for (int i = 0; i < clientEvents.length; i++) {
+			long id = clientEventIds[i];
+			TdApi.Object event = clientEvents[i];
+			boolean mustPrintError = true;
+			if (event instanceof TdApi.Error) {
+				var errorEvent = (TdApi.Error) event;
+				if (Objects.equals("Request aborted", errorEvent.message)) {
+					mustPrintError = false;
+				}
+			}
+			if (mustPrintError) {
+				droppedEvents.add(Map.entry(id, event));
+			}
+		}
+		return droppedEvents;
 	}
 
 	public void registerClient(int clientId, ClientEventsHandler internalClient) {
