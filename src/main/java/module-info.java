873901a5d800654e5f167cc07d5e7a95460f3965
Andrea Cavalli <andrea@cavallium.it>
2021-05-09 22:59:03 +0200

    Update dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module tdlight.java {
 	requires tdlight.api;
 	requires org.reactivestreams;
-	requires it.unimi.dsi.fastutil;
 	requires org.slf4j;
+	requires it.unimi.dsi.fastutil.core;
 	exports it.tdlight.tdlight;
 	exports it.tdlight.tdnative;
 	exports it.tdlight.tdlib;
