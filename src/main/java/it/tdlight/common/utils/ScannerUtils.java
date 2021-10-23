9e7f2c8f751e92f0fcb0047fbd5ac2cd09ae0455
Andrea Cavalli <andrea@cavallium.it>
2021-10-23 23:50:42 +0200

    Remove scanner

--- src/main/java/it/tdlight/common/utils/ScannerUtils.java
+++ src/main/java/it/tdlight/common/utils/ScannerUtils.java
@@ -1,17 +1,8 @@
 package it.tdlight.common.utils;
 
-import java.util.Scanner;
-
 public final class ScannerUtils {
 
-	private static final Scanner scanner;
-
-	static {
-		scanner = new Scanner(System.in);
-	}
-
 	public static String askParameter(String displayName, String question) {
-		System.out.print("[" + displayName + "] " + question + ": ");
-		return scanner.nextLine();
+		return System.console().readLine("[%s] %s", displayName, question);
 	}
 }
