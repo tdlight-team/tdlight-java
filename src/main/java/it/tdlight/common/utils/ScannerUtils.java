52541b0ca86e19221e4caf713e10376a0c23d454
Andrea Cavalli <andrea@cavallium.it>
2021-10-24 00:12:01 +0200

    Use scanner if no console is found

--- src/main/java/it/tdlight/common/utils/ScannerUtils.java
+++ src/main/java/it/tdlight/common/utils/ScannerUtils.java
@@ -1,8 +1,27 @@
 package it.tdlight.common.utils;
 
+import java.io.Console;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
+
 public final class ScannerUtils {
 
+	private static final Object lock = new Object();
+	private static Scanner SCANNER = null;
+
 	public static String askParameter(String displayName, String question) {
-		return System.console().readLine("[%s] %s", displayName, question);
+		Console console = System.console();
+		if (console != null) {
+			return console.readLine("[%s] %s", displayName, question);
+		} else {
+			synchronized (lock) {
+				if (SCANNER == null) {
+					SCANNER = new Scanner(System.in);
+					Runtime.getRuntime().addShutdownHook(new Thread(() -> SCANNER.close()));
+				}
+			}
+			System.out.printf("[%s] %s: ", displayName, question);
+			return SCANNER.nextLine();
+		}
 	}
 }
