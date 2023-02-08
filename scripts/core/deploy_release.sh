8c040112f4456da58f5078ba3405739a45fa5c6e
Andrea Cavalli <andrea@cavallium.it>
2023-02-08 20:22:11 +0100

    Add ssl3 support

--- scripts/core/deploy_release.sh
+++ scripts/core/deploy_release.sh
@@ -1,17 +1,28 @@
 #!/bin/bash -e
 # OTHER REQUIRED ENVIRONMENT VARIABLES:
 #   REVISION = <revision>
+#   SSL_TYPE = <ssl1|ssl3>
 
 # Check variables correctness
 if [ -z "${REVISION}" ]; then
 	echo "Missing parameter: REVISION"
 	exit 1
 fi
+# Check variables correctness
+if [ -z "${SSL_TYPE}" ]; then
+	echo "Missing parameter: SSL_TYPE"
+	exit 1
+fi
+
+SSL_SUFFIX=""
+if [[ "$SSL_TYPE" == "ssl3" ]]; then
+	SSL_SUFFIX="-ssl3"
+fi
 
 cd "../../"
 
 cd "bom"
-mvn -B -Drevision="${REVISION}" clean deploy
+mvn -B -Drevision="${REVISION}" -DrevisionSuffix="" -DnativesSsl3Suffix="${SSL_SUFFIX}" clean deploy
 cd "../"
 
 echo "Done."
