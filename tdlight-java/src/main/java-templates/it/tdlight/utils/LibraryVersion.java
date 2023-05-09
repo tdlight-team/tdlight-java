f5173993ed29c51180193fd9a0150c593b127999
Andrea Cavalli <andrea@cavallium.it>
2023-05-10 00:24:39 +0200

    Fix version number

--- tdlight-java/src/main/java-templates/it/tdlight/utils/LibraryVersion.java
+++ tdlight-java/src/main/java-templates/it/tdlight/utils/LibraryVersion.java
@@ -3,6 +3,6 @@ package it.tdlight.utils;
 public final class LibraryVersion {
 
 	public static final String VERSION = "${project.version}";
-	public static final String NATIVES_VERSION = "4.0.${nativesRevisionNumber}${nativesRevisionSuffix}";
+	public static final String NATIVES_VERSION = "${revision}";
 	public static final String IMPLEMENTATION_NAME = "tdlight";
 }
