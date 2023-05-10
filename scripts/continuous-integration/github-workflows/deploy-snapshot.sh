55a2e7f458389662372afbd6b4b97591b79ac4e5
Andrea Cavalli <andrea@warp.ovh>
2023-05-10 16:31:49 +0200

    Remove unused files

--- scripts/continuous-integration/github-workflows/deploy-snapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-set -e
-# OTHER REQUIRED ENVIRONMENT VARIABLES:
-
-cd ./scripts/core/
-./deploy_snapshot.sh
-
-echo "Done."
-exit 0
--- scripts/continuous-integration/github-workflows/only-compile-release.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-set -e
-# OTHER REQUIRED ENVIRONMENT VARIABLES:
-
-cd ./scripts/core/
-./only_compile_release.sh
-
-echo "Done."
-exit 0
